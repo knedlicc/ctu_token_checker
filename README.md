0be39050a6acfb798eb28ec155a92e39b5b7b5a8
knedlicc <kisstrust@icloud.com>
2024-04-30 19:26:08 +0200

    feat: enchanced python script added instead of initial nodejs version

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# ctu_token_checker
+# Token Checker
+Mini app fetching token balances for a given wallet address. 
+Currently supports only ETH chain. 
+
+## Features
+Wallet Balance Retrieval: The application offers two endpoints for obtaining the balance of a specified Ethereum wallet. The first endpoint retrieves the balance through direct contract calls to the Ethereum network, while the second endpoint utilizes the Ethplorer API for balance retrieval.
+
+## Environment Variables
+The application uses the following environment variables:
+
+**INFURA_API_KEY:** Your Infura API key. This is used to connect to the Ethereum network.
+**ETHPLORER_API_KEY:** Your Ethplorer API key. This is used to fetch data from the Ethplorer API.
+
+## ERC20_ABI
+The application includes the ABI (Application Binary Interface) for ERC20 tokens. This ABI is used to interact with ERC20 token contracts on the Ethereum network. It includes the balanceOf, decimals, and symbol functions.
+
+## Installation
+Python and pip must be intalled on your platform. 
+To install the necessary packages for this script, use pip:
+
+    pip install web3
+    pip install flask
+    pip install python-dotenv
+    pip install pandas
+    pip install requests
+
+## Usage
+To start the application, run the following command:
+
+    py checker.py
+
+
+## API Documentation
+
+This Python script provides two API endpoints for fetching Ethereum wallet balances.
+
+### Direct Balance Fetch
+
+Uses whitelist for token contracts. Resource: [etherscan.io](https://etherscan.io/exportData?type=open-source-contract-codes)
+
+**Endpoint:** /direct/balance/{wallet}
+
+**Method:** GET
+
+**Description:** This endpoint returns the balance of a given Ethereum wallet by making direct contract calls to the Ethereum network.
+
+#### Parameters:
+
+**wallet:** The Ethereum wallet address.
+
+#### Query Parameters:
+
+**chain:** The blockchain network to use. Defaults to 'eth'. `!Note: currently only Ethereum is supported` 
+
+**limit:** The maximum number of tokens to fetch. Must be an integer between 1 and 10000. Defaults to 10.
+
+**Response:** A JSON object containing the balances of all tokens in the wallet. Each key is a token symbol and the corresponding value is the balance of that token.
+
+### Ethplorer API Balance Fetch
+
+No whitelist used for this approach, returns all tokens that have non-zero balance for a given wallet address. `!Note: may return scam tokens as well` 
+
+**Endpoint:** /api/balance/{wallet}
+
+**Method:** GET
+
+**Description:** This endpoint returns the balance of a given Ethereum wallet by fetching data from the Ethplorer API.
+
+#### Parameters:
+
+**wallet:** The Ethereum wallet address.
+
+**Response:** A JSON object containing the balances of all tokens in the wallet. Each key is a token symbol and the corresponding value is the balance of that token.
+
+
+#### Response example:
+
+    {
+        "ETH": 0.123456789,
+        "DAI": 456.789,
+        "USDT": 123456.789
+    }
---
+++ checker.py
@@ -0,0 +1,128 @@
+from web3 import Web3
+from flask import Flask, request
+import os
+from dotenv import load_dotenv
+import pandas as pd
+import requests
+
+load_dotenv()
+
+app = Flask(__name__)
+
+INFURA_API_KEY = os.getenv("INFURA_API_KEY")
+ETHPLORER_API_KEY = os.getenv("ETHPLORER_API_KEY")
+
+
+ERC20_ABI = [
+    # balanceOf
+    {
+        "constant": True,
+        "inputs": [{"name": "_owner", "type": "address"}],
+        "name": "balanceOf",
+        "outputs": [{"name": "balance", "type": "uint256"}],
+        "type": "function"
+    },
+    # decimals
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [{"name": "", "type": "uint8"}],
+        "type": "function"
+    },
+    # symbol
+    {
+        "constant": True,
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [{"name": "", "type": "string"}],
+        "type": "function"
+    }
+]
+
+# Get the balance of a wallet on a specific chain by direct contract calls
+@app.route('/direct/balance/<wallet>', methods=['GET'])
+def get_balances_direct(wallet):    
+    chain, limit = validate_parameters_and_set_defaults(request.args.get('chain'), request.args.get('limit'))
+    balances = {}
+    
+    try:
+        if chain == 'eth':
+            whitelist = fill_whitelist('export-verified-contractaddress-opensource-license.csv',limit)
+            web3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/' + INFURA_API_KEY))
+            eth_balance = web3.eth.get_balance(wallet)
+            balances['ETH'] = float(web3.from_wei(eth_balance, 'ether'))
+            for index, token in whitelist:
+                if(index == limit):
+                    break
+                token = Web3.to_checksum_address(token)
+                contract = web3.eth.contract(address=token, abi=ERC20_ABI)
+                try:
+                    ticker = contract.functions.symbol().call()     
+                    result = contract.functions.balanceOf(wallet).call()
+                    decimals = contract.functions.decimals().call()      
+                except Exception as e:
+                    print(e)
+                    print(f'Error fetching token data: ', token)
+                    continue
+                balances[ticker] = result / (10 ** decimals)
+        else: 
+            return {'error': 'Chain not supported'}, 400   
+    except Exception as e:
+        print(e)
+        return {'error': 'Error fetching balances'}, 500
+    
+    return balances
+    
+# Get the balance of a wallet on a ETH chain by using Ethplorer API
+@app.route('/api/balance/<wallet>', methods=['GET'])
+def get_balances_api(wallet):
+    balances = {}
+    
+    try:
+        response = requests.get(f'https://api.ethplorer.io/getAddressInfo/{wallet}?apiKey={ETHPLORER_API_KEY}')
+        response.raise_for_status()
+        data = response.json()
+        balances['ETH'] = data['ETH']['balance']
+        
+        for token_info in data['tokens']:
+            token = token_info['tokenInfo']
+            balances[token['symbol']] = token_info['balance'] / (10 ** int(token['decimals']))
+    except Exception as e:
+        print(e)
+        return {'error': 'Error fetching balances'}, 500
+    
+    return balances
+
+# Validate the parameters and set the defaults if necessary
+def validate_parameters_and_set_defaults(chain, limit):
+    if chain == None:
+        chain = 'eth'
+        print('Chain not specified, defaulting to ETH')
+        
+    if limit is not None:
+        try: 
+            limit = int(limit)
+        except ValueError:
+            return {'error': 'Limit must be an integer'}, 400
+        if limit < 1 and limit > 10000:
+            return {'error': 'Limit must be greater than 0 and less than 10000'}, 400
+    else:
+        limit = 10
+        print('Limit not specified, defaulting to 10')
+    
+    return chain, limit
+    
+# Fill and get the whitelist with the contract addresses from the CSV file
+def fill_whitelist(filename, limit):
+    whitelist = []
+    df = pd.read_csv(filename, skiprows=2, header=None)
+    for index, row in df.iterrows():
+        if index == limit:
+            break
+        whitelist.append(row[1])  # Assuming the contract address is in the second column
+    return whitelist
+
+# Run the app
+if __name__ == '__main__':
+    app.run(port=3000)
